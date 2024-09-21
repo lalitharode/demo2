1fd47976bb2263aa8e2571ee73d9de5b999653bb
Lalit Kumar harode <lalitharode20141996@gmail.com>
2024-09-21 16:22:51 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+
+setup(
+    name="bike-rent",
+    version="0.0.1",
+    author="lalit",
+    author_email="lalitharode20141996@gmail.com",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "flask"]
+)
